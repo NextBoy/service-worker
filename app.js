4512320fa6b2ae68f3c807e808ec866f78283687
zhuozhongzhi <zhongzhi_z@gzleihou.cn>
2018-02-27 16:28:57 +0800

    init

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 if('serviceWorker' in navigator) {
+    console.log('this is app.js')
     window.addEventListener('load', function () {
         navigator.serviceWorker.register('/service-worker/sw.js', {scope: '/service-worker/'})
             .then(registration => {})
